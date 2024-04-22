6a1b25a56039e27165fc68e9c2612d0a64432101
RAYNZ13 <im.sra13@gmail.com>
2024-04-22 10:00:50 +0530

    3rd commit Setting up the backend connections with fastapi,ngrok to dialogflow's fulfillment

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,38 @@
-print("hi")
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import JSONResponse
+# import dataBaseHelper
+
+app = FastAPI()
+
+@app.post("/")
+async def handle_request(request: Request):
+    #retrive the json data from the request
+    payload = await request.json()
+    
+    #extract the necessary info from payload
+    #based on the structre pf the webhookrequest from dialogflow
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    output_contexts = payload['queryResult']['outputContexts']
+    
+    if intent == 'track order - context: ongoing-tracking':
+        # return track_order(parameters,intent)
+        return JSONResponse(content={
+            "fulfillmentText":f"Recieved =={intent}== in the backend"
+        })
+        
+        
+        
+
+        
+# def track_order(parameters: dict,intent: str):
+#     order_id = parameters['order_id']
+#     order_status = dataBaseHelper.read_order_status(order_id)
+#     if order_status:
+#         fulfilllemt_text = f"The order staus for the order id {order_id} is : {order_status}"
+#     else : 
+#         fulfilllemt_text = f"No order found for the order id {order_id}"   
+#     return JSONResponse(
+#         content = {"fulfillmentText":f"Recieved =={intent}== in the backend"}
+#     )
